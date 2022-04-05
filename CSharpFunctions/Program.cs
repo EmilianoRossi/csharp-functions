f3e9f76fe45e40b8a22ecf72864a2d626b7982c4
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-04-05 14:45:48 +0200

    Esercizio 2 , funzione stampa il quadrato di un numero completato.

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -23,10 +23,36 @@ void StampaArrayInteri(int[] array)
 
     }
 
-    Console.Write("]");
+    Console.WriteLine("]");
 }
+
+
+//ESERCIZIO 2 STAMPA IL QUADRATO DI UN NUMERO PASSATO COME PARAMETRO
+
+
+
+int Quadrato(int numero)
+{
+
+    int quadrato = (int)Math.Pow(numero, 2);
+    return quadrato;
+
+}
+
+
+
+
+
+
 
 //RICHIAMO LE FUNZIONI
 
+//richiamo funzione stampa array
 int[] arrayDiNumeri = { 2, 6, 7, 5, 3, 9 };
 StampaArrayInteri(arrayDiNumeri);
+
+//richiamo funzione quadrato di un numero
+Console.WriteLine("Inserisci un numero da elevare al quadrato");
+int mioNumero = int.Parse(Console.ReadLine());
+int quadratoNumero = Quadrato(mioNumero);
+Console.WriteLine(quadratoNumero);
