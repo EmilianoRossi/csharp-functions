142e5378bb939c5cf71e815de926d40b4bc60414
EmilianoRossi <emiliano.rossi658@gmail.com>
2022-04-05 14:35:34 +0200

    Esercizio 1, funzione stampa array terminato

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿//ESERCIZIO 1 STAMPA ARRAY SECONDO IL FORMATO [n , n , n , n]
+
+void StampaArrayInteri(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (i < array.Length-1)
+        {
+
+            Console.Write(array[i] + " , ");
+
+        }
+
+        else
+        {
+
+            Console.Write(array[i]);
+
+        }
+
+    }
+
+    Console.Write("]");
+}
+
+//RICHIAMO LE FUNZIONI
+
+int[] arrayDiNumeri = { 2, 6, 7, 5, 3, 9 };
+StampaArrayInteri(arrayDiNumeri);
